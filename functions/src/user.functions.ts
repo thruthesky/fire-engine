64ff80109496de12c8e0b3c02ec748da379b94b1
JaeHo Song <thruthesky@gmail.com>
2024-05-29 13:55:08 +0800

    firebase extensions for fireflutter fisrt import

---
+++ functions/src/user.functions.ts
@@ -0,0 +1,16 @@
+import { onRequest } from "firebase-functions/v1/https";
+import * as logger from "firebase-functions/logger";
+import { getDatabase } from "firebase-admin/database";
+
+export const userUpdate = onRequest(async (request, response) => {
+    logger.info(`LOG: userUpdate() begins with request: ${request.query.name}`);
+
+    const uid = request.query.uid;
+    const name = request.query.name;
+
+    const rtdb = getDatabase();
+    await rtdb.ref(`users/${uid}`).update({ name });
+
+    response.send({ code: 200, message: "OK" });
+});
+
---
+++ functions/tests/user/user-count.spec.ts
@@ -0,0 +1,15 @@
+import { expect } from "chai";
+import "mocha";
+import * as admin from "firebase-admin";
+import { initializeFirebaseOnce } from "../initialize-firebase-once";
+
+initializeFirebaseOnce();
+
+describe("Count no of users", () => {
+    it("Should be bigger than 0", async () => {
+        const rtdb = admin.database();
+        const snapshot = await rtdb.ref("users").get();
+        console.log("snapshot.numChildren()", snapshot.numChildren());
+        return expect(snapshot.numChildren()).greaterThan(0);
+    });
+});
