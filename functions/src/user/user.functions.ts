ef1af2a5e87ecccf69098b465b7214d14cdad0b9
JaeHo Song <thruthesky@gmail.com>
2024-05-30 21:30:15 +0800

    user functions

--- functions/src/user/user.functions.ts
+++ functions/src/user/user.functions.ts
@@ -1,24 +1,46 @@
-import { onRequest } from "firebase-functions/v1/https";
-import * as logger from "firebase-functions/logger";
-import { getDatabase } from "firebase-admin/database";
+import * as functions from "firebase-functions";
+import { Config } from "../config";
+import { isCreate, isDelete, isUpdate } from "../library";
+
+import { ServerValue, getDatabase } from "firebase-admin/database";
+
 
 // phoneNumberRegister
 
 // userLike
 
-// userDeleteAccount => RTDB + HTTP call 로 한다. callable 로 하지 않는다.
-
-export const userUpdate = onRequest(async (request, response) => {
-  logger.info(`LOG: userUpdate() begins with request: ${request.query.name}`);
-
-  const uid = request.query.uid;
-  const name = request.query.name;
-
-  const rtdb = getDatabase();
-  await rtdb.ref(`users/${uid}`).update({ name });
-
-  response.send({ code: 200, message: "OK" });
-});
+// userDeleteAccount => RTDB Event Trigger 로 하는데, Flutter 에서는 Completer 로 작성한다. 타임아웃 30초로 둔다.
 
 
+/**
+ * Post write 이벤트가 발생하면, 해당 포스트의 요약 정보를 업데이트 한다.
+ */
+export const userLike = functions.database.ref(`${Config.whoILike}/{myUid}/{targetUid}`)
+  .onWrite(async (change: functions.Change<functions.database.DataSnapshot>, context: functions.EventContext<{
+    myUid: string;
+    targetUid: string;
+  }>) => {
+    const db = getDatabase();
+    const myUid = context.params.myUid;
+    const targetUid = context.params.targetUid;
+
+    // created or updated
+    if (isCreate(change) || isUpdate(change)) {
+      await db.ref(`${Config.whoLikeMe}/${targetUid}`).update({ [myUid]: true });
+      await db.ref(`users/${targetUid}`).update({ noOfLikes: ServerValue.increment(1) });
+    } else if (isDelete(change)) {
+      // deleted
+      await db.ref(`${Config.whoLikeMe}/${targetUid}/${myUid}`).remove();
+      await db.ref(`users/${targetUid}`).update({ noOfLikes: ServerValue.increment(-1) });
+    }
+
+    /// TODO - 여기서 부터, 푸시 알림 전송하기
+    // Send message to the target user
+    // if (isCreate(event)) {
+    //   await MessagingService.sendMessageWhenUserLikeMe({
+    //     uid: targetUid,
+    //     otherUid: myUid,
+    //   });
+    // }
+  });
 
