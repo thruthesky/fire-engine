0c68c192fface57989167c47624e1e269b29c3af
Christian Bucal <withcenter-dev2@github.com>
2024-06-11 13:33:46 +0800

    initial check of codes in link

--- functions/src/messaging/messaging.interfaces.ts
+++ functions/src/messaging/messaging.interfaces.ts
@@ -1,11 +1,12 @@
 
+// eslint-disable-next-line max-len
 import {AndroidConfig, ApnsConfig, FcmOptions, WebpushConfig} from "firebase-admin/messaging";
-
 
 /**
  * Basic interface for sending a message
  *
- * Note that, the image is optional and is not the required field for sending a message
+ * Note that, the image is optional and is not the required
+ * field for sending a message
  */
 export interface MessageNotification {
     title: string;
@@ -21,9 +22,9 @@ export interface MessageRequest extends MessageNotification {
     data?: { [key: string]: string };
 }
 
-
 /**
- * A message data interface for sending push notification when a new post is created.
+ * A message data interface for sending push notification
+ * when a new post is created.
  */
 export interface PostCreateMessage {
     // 글 아이디
@@ -56,7 +57,6 @@ export interface MessagePayload {
     fcmOptions?: FcmOptions;
 }
 
-
 /**
  * Inteface for sendNotificationToUids
  *
@@ -72,17 +72,19 @@ export interface NotificationToUids {
     body: string;
     image?: string;
     data?: { [key: string]: string };
-
     // 푸시 알림을 전송하는 목적.
     // - 로그를 기록할 때 사용한다.
-    // 이 값에는 'post', 'comment', 'like', 'chat', 'profile' 등 어떤 액션에 대한 알림인지를 지정한다.
-    // 'post' 는 새 글 작성 알림, 'comment' 는 새 코멘트 작성 알림, 'like' 는 좋아요 알림, 'chat' 은 채팅 알림,
+    // 이 값에는 'post', 'comment', 'like', 'chat', 'profile'
+    // 등 어떤 액션에 대한 알림인지를 지정한다.
+    // 'post' 는 새 글 작성 알림, 'comment' 는 새 코멘트 작성 알림,
+    // 'like' 는 좋아요 알림, 'chat' 은 채팅 알림,
     action: string;
     // 알림을 받는 대상의 객체 ID 이다.
-    // action 이 'post' 이면 글 ID, 'comment' 이면 comment ID, 'like' 이면 like 를 받는 상대방의 ID, 'chat' 이면 채팅방 ID, 'profile' 이면 프로필 사용자 ID 이다.
+    // action 이 'post' 이면 글 ID, 'comment' 이면 comment ID,
+    // 'like' 이면 like 를 받는 상대방의 ID, 'chat' 이면 채팅방 ID,
+    // 'profile' 이면 프로필 사용자 ID 이다.
     targetId: string;
 }
-
 
 /**
  * Interface for sending message when a user likes another user
@@ -91,7 +93,6 @@ export interface UserLikeEvent {
     senderUid: string;
     receiverUid: string;
 }
-
 
 // export interface UserLikeEventForNotification {
 //     uid: string;
